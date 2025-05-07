86940bc142f3e5d9c8e98ec5163de494d84d26ea
AndresFForeroP <andres2200500@correo.uis.edu.co>
2025-05-07 17:16:31 -0500

    falta funcionalidad de botones y que sea responsiva

--- app.js
+++ app.js
@@ -22,16 +22,16 @@ class Barrabusqueda extends HTMLElement{
                   }
                 .barra-busq{
                     margin-left: 30vw;
-                    background-color: var(----primercolor);
+                    background-color: var(----tercercolor);
                     border: 2px solid black;
                     border-radius: 0.6rem;
                     height: 4rem;
                     width: 40rem;
                     form{display: grid;
                         grid-template-columns: 25% 60% 19%;
-                        color: var(----quintocolor);
+                        color: var(----cuartocolor);
                         font-size: 1.5rem;
-                        text-shadow: 2px 2px black;}
+                        text-shadow: 3px 3px 5px black;}
                         input{
                             margin-top: 0.9rem;
                             margin-bottom: 2.5rem;
@@ -60,4 +60,66 @@ class Barrabusqueda extends HTMLElement{
             </div>`;
     }
 }
-customElements.define('barra-busqueda',Barrabusqueda)
+class MostrarHeroes extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow ({mode:'open'});
+        let cadena = /*css*/`
+        <style>
+        .Heroe{
+            margin: 1.3rem;
+            margin-top: 3rem;
+            border: 3px solid var(----primercolor);
+            border-radius: 1.5rem;
+            background-color: var(----segundocolor);
+            text-align: center;
+            img{
+                max-width: 13vw;
+                max-height:16vw;
+                border: 2px solid black;
+            }
+            .titulo-heroe{
+                color: var(----cuartocolor);
+                text-shadow: 3px 3px 3px black;
+                font-weight: 100;
+                font-size: 3vw;
+            }
+            p{
+                font-size: 1.8rem;
+                margin: 0.8rem;
+                color: #fedb0e;
+                text-shadow: 2px 2px 2px var(----quintocolor);
+            }
+            p:nth-child(6){
+                color: black;
+                text-shadow: 2px 2px 3px white;
+                font-size: 1.3rem;
+            }
+            button{
+                background-color: var(----tercercolor);
+                font-size: 2rem;
+                color: var(----cuartocolor);
+                margin: 1rem;
+                padding: 0.4rem;
+                width: 9rem;
+                text-shadow: 2px 2px 3px var(----primercolor);
+            }
+        }
+        </style>`
+        heroes.forEach(heroe => {
+            cadena += /*html*/`
+            <div class="Heroe">
+                <h1 class="titulo-heroe">${heroe.nombre}</h1>
+                <img src=${heroe.img} alt="">
+                <p>${heroe.clave}</p>
+                <p>${heroe.casa}</p>
+                <p>${heroe.año}</p>
+                <p>${heroe.descripcion}</p>
+                <button>Ver mas</button>
+            </div>`;
+        });
+        this.shadowRoot.innerHTML = cadena
+    }
+}
+customElements.define('barra-busqueda',Barrabusqueda);
+customElements.define('lista-heroes',MostrarHeroes);
--- data/heroes.js
+++ data/heroes.js
@@ -1,1 +1,162 @@
-export const heroes = {}
+export const heroes = [
+    {
+      "nombre": "Peter Parker",
+      "img": "img/spider-man.jpg",
+      "clave": "Spider-Man",
+      "casa": "Marvel",
+      "año": "1962",
+      "descripcion": "Spider-Man es conocido por su ingenio rápido, su sentido del humor en situaciones de peligro y su perseverancia inquebrantable"
+    },
+    {
+      "nombre": "Bruce Wayne",
+      "img": "img/batman.jpg",
+      "clave": "Batman",
+      "casa": "DC",
+      "año": "1939",
+      "descripcion": "Batman lucha contra el crimen utilizando su intelecto, habilidades físicas y un vasto arsenal de tecnología"
+    },
+    {
+      "nombre": "Clark Kent",
+      "img": "img/superman.jpg",
+      "clave": "Superman",
+      "casa": "DC",
+      "año": "1938",
+      "descripcion": "Superman es el símbolo de esperanza, justicia y fuerza moral, con poderes casi ilimitados"
+    },
+    {
+      "nombre": "Tony Stark",
+      "img": "img/iron-man.jpg",
+      "clave": "Iron Man",
+      "casa": "Marvel",
+      "año": "1963",
+      "descripcion": "Iron Man es un genio multimillonario que combate el mal con una armadura de alta tecnología"
+    },
+    {
+      "nombre": "Diana Prince",
+      "img": "img/wonder-woman.jpg",
+      "clave": "Wonder Woman",
+      "casa": "DC",
+      "año": "1941",
+      "descripcion": "Wonder Woman es una guerrera amazona con fuerza sobrehumana y un fuerte sentido de la justicia"
+    },
+    {
+      "nombre": "Steve Rogers",
+      "img": "img/captain-america.jpg",
+      "clave": "Captain America",
+      "casa": "Marvel",
+      "año": "1941",
+      "descripcion": "Captain America representa los valores de coraje, liderazgo y sacrificio al servicio de la libertad"
+    },
+    {
+      "nombre": "Barry Allen",
+      "img": "img/flash.jpg",
+      "clave": "Flash",
+      "casa": "DC",
+      "año": "1956",
+      "descripcion": "Flash es el hombre más rápido del mundo, usando su velocidad para combatir el crimen y viajar en el tiempo"
+    },
+    {
+      "nombre": "Bruce Banner",
+      "img": "img/hulk.jpg",
+      "clave": "Hulk",
+      "casa": "Marvel",
+      "año": "1962",
+      "descripcion": "Hulk es la manifestación del poder descontrolado, resultado de la furia del Dr. Bruce Banner"
+    },
+    {
+      "nombre": "Hal Jordan",
+      "img": "img/green-lantern.jpg",
+      "clave": "Green Lantern",
+      "casa": "DC",
+      "año": "1959",
+      "descripcion": "Green Lantern utiliza un anillo de poder para crear lo que imagine, guiado por la fuerza de su voluntad"
+    },
+    {
+      "nombre": "Thor Odinson",
+      "img": "img/thor.jpg",
+      "clave": "Thor",
+      "casa": "Marvel",
+      "año": "1962",
+      "descripcion": "Thor es el dios del trueno, defensor de Asgard y la Tierra con su martillo Mjolnir"
+    },
+    {
+      "nombre": "Natasha Romanoff",
+      "img": "img/black-widow.jpg",
+      "clave": "Black Widow",
+      "casa": "Marvel",
+      "año": "1964",
+      "descripcion": "Black Widow es una espía experta y combatiente letal, con un pasado oscuro redimido por su heroísmo"
+    },
+    {
+      "nombre": "Arthur Curry",
+      "img": "img/aquaman.jpg",
+      "clave": "Aquaman",
+      "casa": "DC",
+      "año": "1941",
+      "descripcion": "Aquaman es el rey de la Atlántida, capaz de comunicarse con criaturas marinas y controlar el océano"
+    },
+    {
+      "nombre": "Logan",
+      "img": "img/wolverine.jpg",
+      "clave": "Wolverine",
+      "casa": "Marvel",
+      "año": "1974",
+      "descripcion": "Wolverine posee garras retráctiles y un poderoso factor curativo que lo hace casi inmortal"
+    },
+    {
+      "nombre": "Jean Grey",
+      "img": "img/phoenix.jpg",
+      "clave": "Phoenix",
+      "casa": "Marvel",
+      "año": "1963",
+      "descripcion": "Phoenix es una mutante con habilidades telepáticas y telequinéticas, vinculada a una fuerza cósmica destructiva"
+    },
+    {
+      "nombre": "Stephen Strange",
+      "img": "img/doctor-strange.jpg",
+      "clave": "Doctor Strange",
+      "casa": "Marvel",
+      "año": "1963",
+      "descripcion": "Doctor Strange es el Hechicero Supremo, maestro de las artes místicas y defensor del plano astral"
+    },
+    {
+      "nombre": "Selina Kyle",
+      "img": "img/catwoman.jpg",
+      "clave": "Catwoman",
+      "casa": "DC",
+      "año": "1940",
+      "descripcion": "Catwoman es una ladrona ágil y astuta con un código moral propio y una relación complicada con Batman"
+    },
+    {
+      "nombre": "Scott Lang",
+      "img": "img/ant-man.jpg",
+      "clave": "Ant-Man",
+      "casa": "Marvel",
+      "año": "1979",
+      "descripcion": "Ant-Man puede cambiar de tamaño gracias a la tecnología Pym, usando su astucia para combatir el crimen"
+    },
+    {
+      "nombre": "Barbara Gordon",
+      "img": "img/batgirl.jpg",
+      "clave": "Batgirl",
+      "casa": "DC",
+      "año": "1967",
+      "descripcion": "Batgirl es una experta combatiente y hacker brillante, comprometida con proteger Gotham"
+    },
+    {
+      "nombre": "T'Challa",
+      "img": "img/black-panther.jpg",
+      "clave": "Black Panther",
+      "casa": "Marvel",
+      "año": "1966",
+      "descripcion": "Black Panther es el rey de Wakanda, guerrero imparable con tecnología avanzada y habilidades felinas"
+    },
+    {
+      "nombre": "John Constantine",
+      "img": "img/constantine.jpg",
+      "clave": "Constantine",
+      "casa": "DC",
+      "año": "1985",
+      "descripcion": "Constantine es un cínico ocultista británico que usa magia, astucia y engaños para enfrentarse a lo sobrenatural"
+    }
+  ];
